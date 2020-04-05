5d2924a51dedb37fba6d429f17b00476e3e613f5
davispc10 <davispc100@gmail.com>
2020-04-05 00:47:21 -0300

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://gobarber-back.herokuapp.com/',
+  baseURL: 'https://gobarber-back.herokuapp.com',
 });
 
 export default api;
